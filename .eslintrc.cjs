042371b1874b33a53796ee64fe8fba6522fe2c5d
Paul Andrew <75168528+NotKaskus@users.noreply.github.com>
2024-07-22 11:03:55 +0000

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+// eslint-disable-next-line tsdoc/syntax
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	root: true,
+	extends: ['@kbotdev/eslint-config', 'turbo'],
+	parserOptions: {
+		project: './tsconfig.eslint.json',
+		tsconfigRootDir: __dirname
+	},
+	rules: {
+		'@typescript-eslint/no-unused-vars': 'error'
+	}
+};
